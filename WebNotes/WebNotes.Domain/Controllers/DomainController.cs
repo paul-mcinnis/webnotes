82326a139d5ae5e403926f11459c0ef034089396
Paul <mcinnis.paul@gmail.com>
2018-01-26 19:03:26 +0000

    renamed values controller DomainController

---
+++ WebNotes/WebNotes.Domain/Controllers/DomainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebNotes.Domain.Controllers
+{
+
+    public class DomainController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Domain Api Works!";
+        }
+
+    }
+}
